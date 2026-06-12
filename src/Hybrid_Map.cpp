5571ee5963c931e00ef56a18014921491de82778
Naikekongwo <2864364583@qq.com>
2026-06-12 18:47:33 +0800

    · 移除了一些无水之境的遗产

--- src/Hybrid_Map.cpp
+++
@@ -1,9 +0,0 @@
-// #include "OpenCore.hpp"
-
-// void Hybrid_Map::onEnter()
-// {
-//     // 初始化
-//     this->WindowHeight = OpenCoreManagers::SetManager.getRenderHeight();
-//     this->WindowWidth = OpenCoreManagers::SetManager.getRenderWidth();
-//     // 藉此已经得到了每次需要渲染的区块的大小
-// }
